21378bb3c0ade7ee3fdf0dd7299a7fd324ce7df6
l-pizarro <leandro.pizarro@usach.cl>
2019-06-21 09:56:39 -0400

    Test

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -3,7 +3,7 @@
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
         <el-menu-item index="1">Reservas</el-menu-item>
         <el-menu-item index="2">Registro</el-menu-item>
-        <el-menu-item index="3">Ayuda</el-menu-item>
+        <el-menu-item index="3">Ayuda 2019</el-menu-item>
     </el-menu>
 </div>
 </template>
